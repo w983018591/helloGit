87d729647ec119b1d1d27d3d1eba22f5277f8baa
w983018591 <983018591@aliyun.com>
2022-11-05 00:41:47 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/guigu/git/HelloGit.java
@@ -0,0 +1,14 @@
+package com.guigu.git;
+
+/**
+ * @author yubin
+ * Description
+ * @create 2022-11-05 0:32
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+
+System.out.println("hello git");
+    }
+
+}
---
+++ HelloGit/src/main/java/com/guigu/git/Main.java
@@ -0,0 +1,13 @@
+package com.guigu.git;
+
+/**
+ * @author ${USER}
+ * Description
+ * @create ${YEAR}-${MONTH}-${DAY} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
