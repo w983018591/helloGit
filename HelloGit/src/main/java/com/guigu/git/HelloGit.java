dde419eaae9e4343fb8e03a736e050d2ef6c35cf
w983018591 <983018591@aliyun.com>
2022-11-05 00:44:11 +0800

    更新1

--- HelloGit/src/main/java/com/guigu/git/HelloGit.java
+++ HelloGit/src/main/java/com/guigu/git/HelloGit.java
@@ -7,8 +7,8 @@ package com.guigu.git;
  */
 public class HelloGit {
     public static void main(String[] args) {
-
-System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("更新1");
     }
 
 }
