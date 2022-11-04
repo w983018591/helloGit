072807c63507d4a87cbff41a9d4c8742e2e4a542
w983018591 <983018591@aliyun.com>
2022-11-05 00:58:36 +0800

    主干添加

--- HelloGit/src/main/java/com/guigu/git/HelloGit.java
+++ HelloGit/src/main/java/com/guigu/git/HelloGit.java
@@ -9,7 +9,7 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("更新1");
-        System.out.println("创建分支");
+        System.out.println("主干添加");
     }
 
 }
